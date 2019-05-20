18efc5f4a033b77f4216314737c87fd91cae6588
Bhavin Joshi <bajoshi@asu.edu>
2019-05-20 11:38:26 -0500

    More updates to covariance matrix estimation

--- gaussianprocesses/covmat_test.py
+++ gaussianprocesses/covmat_test.py
@@ -13,11 +13,18 @@ import os
 
 import matplotlib.pyplot as plt
 
+home = os.getenv('HOME')
+pears_data = home + '/Documents/PEARS/data_spectra_only/'
+massive_galaxies_dir = home + '/Desktop/FIGS/massive-galaxies/'
+
+sys.path.append(massive_galaxies_dir + 'grismz_pipeline/')
+import new_refine_grismz_gridsearch_parallel as ngp
+
 def get_covmat(spec_wav, spec_flux, spec_ferr):
 
     # First define the kernel that will be used to model
     # the off-diagonal elements of the covariance matrix.
-    galaxy_len_fac = 40
+    galaxy_len_fac = 35
     # galaxy_len_fac includes the effect in correlation due to the 
     # galaxy morphology, i.e., for larger galaxies, flux data points 
     # need to be farther apart to be uncorrelated.
@@ -27,9 +34,8 @@ def get_covmat(spec_wav, spec_flux, spec_ferr):
     # i.e., this amount of correlation in hte noise will 
     # exist even for a point source
     kern_len_fac = base_fac + galaxy_len_fac
-    kernel = george.GP(kernel=np.var(spec_flux) * kernels.ExpSquaredKernel(kern_len_fac))
-
-    print "Variance in flux:", np.var(spec_flux)
+    #kernel = george.GP(kernel=np.var(spec_flux) * kernels.ExpSquaredKernel(kern_len_fac))
+    #print "Variance in flux:", np.var(spec_flux)
 
     """
     kernel.compute(x=spec_wav, yerr=spec_ferr)
@@ -52,18 +58,31 @@ def get_covmat(spec_wav, spec_flux, spec_ferr):
     covmat = np.identity(N)
 
     # Now populate the elements of the matrix
-    len_fac = -1 / (2 * kern_len_fac)
+    len_fac = -1 / (2 * kern_len_fac**2)
+    theta_0 = max(spec_ferr)**2
+    print "Theta_0 is:", theta_0
+    print "2x Theta_0 is:", 2*theta_0
     for i in range(N):
         for j in range(N):
 
             if i == j:
-                covmat[i,j] = spec_ferr[i]
+                covmat[i,j] = theta_0 + spec_ferr[i]**2
             else:
-                covmat[i,j] = np.exp(len_fac * (spec_wav[i] - spec_wav[j])**2)
+                covmat[i,j] = theta_0 * np.exp(len_fac * (spec_wav[i] - spec_wav[j])**2)
 
+    # PLot 
     fig = plt.figure()
     ax = fig.add_subplot(111)
+
+    ax.set_xlabel('Spectral element index')
+    ax.set_ylabel('Spectral element index')
+
+    #covmat = np.log10(covmat)
+    # Set everything below a certain lower limit to exactly zero
+    inv_idx = np.where(covmat <= 1e-4 * theta_0)
+    covmat[inv_idx] = np.nan
     cax = ax.imshow(covmat)
+    
     fig.colorbar(cax)
     plt.show()
 
@@ -72,9 +91,13 @@ def get_covmat(spec_wav, spec_flux, spec_ferr):
 def main():
 
     # Read in a test PEARS grism spectrum
-    hdu = fits.open(pears_data + )
+    current_id = 100543
+    current_field = 'GOODS-S'
+    grism_lam_obs, grism_flam_obs, grism_ferr_obs, pa_chosen, netsig_chosen, return_code = \
+    ngp.get_data(current_id, current_field)
 
     # Tryout a fake spectrum as well
+    """
     np.random.seed(12345)
     rng = (-10, 10)
     num = 100
@@ -85,11 +108,14 @@ def main():
     # Plot the spectrum for a quick eyeball check
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.errorbar(test_wavl, test_flux, yerr=test_ferr)
+    #ax.errorbar(test_wavl, test_flux, yerr=test_ferr)
+    ax.errorbar(grism_lam_obs, grism_flam_obs, yerr=grism_ferr_obs)
     plt.show()
+    sys.exit(0)
+    """
 
     # Get covariance matrix
-    get_covmat(test_wavl, test_flux, test_ferr)
+    get_covmat(grism_lam_obs, grism_flam_obs, grism_ferr_obs)
 
     return None
 
