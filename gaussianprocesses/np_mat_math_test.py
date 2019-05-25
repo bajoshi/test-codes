3a48c3e22cb80059755dc239359d391a3cd6a601
Bhavin Joshi <bajoshi@asu.edu>
2019-05-25 08:06:55 -0500

    Update on alpha computation

--- gaussianprocesses/np_mat_math_test.py
+++ gaussianprocesses/np_mat_math_test.py
@@ -60,12 +60,41 @@ if __name__ == '__main__':
     # For the purposes of comparison I'm simply using 
     # samples drawn from astropy's gaussian + line model.
     # This is simply a numpy array.
-
+    model = np.arange(N)
 
     # Now compute alpha explicitly
+    num = 0
+    den = 0  
+    for i in range(N):
+        for j in range(N):
+
+            num += (dat[i] * model[j] + dat[j] * model[i]) * covmat_inv[i,j]
+            if i == j:
+                kron_delt = 1
+            else:
+                kron_delt = 0
+            den += model[i] * model[j] * covmat_inv[i,j] * (1 + kron_delt)
+
+    alpha_ = num / den
+    print "Alpha computed explicitly:", alpha_
+
+    # Compute alpha using numpy arrays without explicit for loops
+
+
+    # Compute chi2
+    chi2_ = np.matmul((dat - alpha_ * model), np.matmul(covmat_inv, (dat - alpha_ * model)))
+
+    print np.matmul(covmat_inv, (dat - alpha_ * model)).shape
+    print chi2_
+
+    # Chi2 and alpha computed without any correlation taken into account
+    alpha_nocorr = np.sum(dat * model / (daterr**2)) / np.sum(model**2 / daterr**2)
+    chi2_nocorr = np.sum(((dat - alpha_nocorr * model) / daterr)**2)
+
+    print alpha_nocorr
+    print chi2_nocorr
 
     sys.exit(0)
 
 
 
-    
